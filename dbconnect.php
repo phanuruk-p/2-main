f1942a323dbbc9de42f988597d7de1c178e128d5
66040233150 <66040233150@udru.ac.th>
2025-01-28 10:50:35 +0700

    first commit

---
+++ dbconnect.php
@@ -0,0 +1,6 @@
+<!--<link href="css/style.css" rel="stylesheet">-->
+<?php
+$con = mysqli_connect("localhost", "its66040233150", "K9fgT7A8", "its66040233150") or die("เกิดข้อผิดพลาดเกิดขึ้น");
+
+mysqli_set_charset($con, "utf8");
+?>
